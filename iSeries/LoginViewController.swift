794e878a1b21dac7968204d2c05205ae4707bfc6
Alex <alexcris.m@gmail.com>
2015-03-04 22:59:31 -0600

    Commit

---
+++ iSeries/LoginViewController.swift
@@ -0,0 +1,53 @@
+//
+//  LoginViewController.swift
+//  iSeries
+//
+//  Created by Alejandro Cristerna on 3/4/15.
+//  Copyright (c) 2015 ITESM. All rights reserved.
+//
+
+import UIKit
+
+protocol LoginViewControllerDelegate {
+    func loginViaFacebook(loginViewController : LoginViewController)
+}
+
+class LoginViewController: UIViewController {
+    
+    var delegate : LoginViewControllerDelegate?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func loginViaFacebook(sender: AnyObject) {
+        let permissionsArray = ["user_about_me"]
+        
+        PFFacebookUtils.logInWithPermissions(permissionsArray, block: { (user, error) -> Void in
+            if user == nil {
+                println("error")
+            } else {
+                if user.isNew {
+                    println("User signed up and login")
+                } else {
+                    println("User logged in")
+                }
+                
+                if self.delegate != nil {
+                    self.delegate!.loginViaFacebook(self)
+                }
+            }
+            
+            
+        })
+        
+    }
+}
