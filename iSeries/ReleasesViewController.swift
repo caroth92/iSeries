d8487b9e10d5e792146fa3a80f120bff14bd89dd
Carolina De La Torre <caro@schoolhub.com>
2015-03-07 20:58:14 -0600

    Tabs navigation

---
+++ iSeries/ReleasesViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ReleasesViewController.swift
+//  iSeries
+//
+//  Created by Carolina De La Torre on 3/7/15.
+//  Copyright (c) 2015 ITESM. All rights reserved.
+//
+
+import UIKit
+
+class ReleasesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
