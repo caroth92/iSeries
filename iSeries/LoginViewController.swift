4ef6a63e41adce98c048f54d31b32c6d331b11de
Carolina De La Torre <caroth92@users.noreply.github.com>
2015-03-07 20:39:45 -0600

    Revert "Fixes #12 - Email login/signup"

--- iSeries/LoginViewController.swift
+++ iSeries/LoginViewController.swift
@@ -14,18 +14,12 @@ protocol LoginViewControllerDelegate {
 
 class LoginViewController: UIViewController {
     
-    @IBOutlet weak var userEmail: UITextField!
-    @IBOutlet weak var userPassword: UITextField!
-    
-    
     var delegate : LoginViewControllerDelegate?
-    
-    //#Mark ------------------------------------------------------------------------------------------------------------------------
-    //#Mark - View Lifecycle
-    //#Mark ------------------------------------------------------------------------------------------------------------------------
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,40 +27,20 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    //#Mark ------------------------------------------------------------------------------------------------------------------------
-    //#Mark - Login Via Email
-    //#Mark ------------------------------------------------------------------------------------------------------------------------
-    
-    @IBAction func loginViaEmail(sender: AnyObject) {
-        // TODO: Save and send credentials
-        
-        PFUser.logInWithUsernameInBackground(self.userEmail.text, password:self.userPassword.text) {
-            (user: PFUser!, error: NSError!) -> Void in
-            if user != nil {
-                let storyboard = UIStoryboard(name: "Main", bundle: nil)
-                let tabViewController = storyboard.instantiateViewControllerWithIdentifier("TabController") as UIViewController
-                self.presentViewController(tabViewController, animated: true, completion: nil)
-            } else {
-                var alert = UIAlertController(title: "Login failed", message: "Sorry, the login failed. Please try again", preferredStyle: UIAlertControllerStyle.Alert)
-                alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
-                self.presentViewController(alert, animated: true, completion: nil)
-            }
-        }
-    }
-    
-    //#Mark ------------------------------------------------------------------------------------------------------------------------
-    //#Mark - Login Via Facebook
-    //#Mark ------------------------------------------------------------------------------------------------------------------------
-    
+
     @IBAction func loginViaFacebook(sender: AnyObject) {
         let permissionsArray = ["user_about_me"]
         
         PFFacebookUtils.logInWithPermissions(permissionsArray, block: { (user, error) -> Void in
 
-            if((error) != nil) {
+            if user.isNew {
+                println("User signed up and login")
+            } else {
                 let storyboard = UIStoryboard(name: "Main", bundle: nil)
                 let seriesVc = storyboard.instantiateViewControllerWithIdentifier("TabController") as UIViewController
                 self.presentViewController(seriesVc, animated: true, completion: nil)
+
+                println("User logged in")
             }
                 
             if self.delegate != nil {
