c1ed06e05878bd3b050dfc53759737938468a469
Carolina De La Torre <caro@schoolhub.com>
2015-05-17 18:29:37 -0500

    Set episode watched/unwatched

--- iSeries/MyEpisodesViewController.swift
+++ iSeries/MyEpisodesViewController.swift
@@ -11,9 +11,10 @@ import UIKit
 class MyEpisodesViewController: PFQueryTableViewController {
     
     var serie: PFObject!
+    var userEpisodes: NSMutableDictionary = NSMutableDictionary()
     //var serie: NSString!
-    var seasons = NSMutableArray()
-    var episodes = NSMutableArray()
+    //var seasons = NSMutableArray()
+    //var episodes = NSMutableArray()
     
     override init(style: UITableViewStyle, className: String!) {
         super.init(style: style, className: className)
@@ -23,12 +24,12 @@ class MyEpisodesViewController: PFQueryTableViewController {
         super.init(coder: aDecoder)
         
         // Configure the PFQueryTableView
-        self.parseClassName = "UserHistorial"
+        self.parseClassName = "Episodio"
         self.textKey = "Titulo"
         self.pullToRefreshEnabled = true
         self.paginationEnabled = false
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -42,9 +43,9 @@ class MyEpisodesViewController: PFQueryTableViewController {
         var queryEpisodes = PFQuery(className: "Episodio")
         
         for season in self.seasons {
-            queryEpisodes.whereKey("Temporada", equalTo: (season.objectId as String?)!)
-            let results = queryEpisodes.findObjects()
-            self.episodes.addObjectsFromArray(results!)
+        queryEpisodes.whereKey("Temporada", equalTo: (season.objectId as String?)!)
+        let results = queryEpisodes.findObjects()
+        self.episodes.addObjectsFromArray(results!)
         }
         
         println(episodes.count)*/
@@ -69,16 +70,38 @@ class MyEpisodesViewController: PFQueryTableViewController {
         return episodes
     }
     
+    override func objectsDidLoad(error: NSError!) {
+        super.objectsDidLoad(error)
+        
+        // Add episode's seen state
+        for object in self.objects! {
+            var query = PFQuery(className: "UserHistorial")
+            query.whereKey("episodio", equalTo: object)
+            query.whereKey("user", equalTo: PFUser.currentUser()!)
+            query.getFirstObjectInBackgroundWithBlock{
+                (object: PFObject?, error: NSError?) -> Void in
+                if error == nil && object != nil {
+                    let episode = object!["episodio"] as? PFObject
+                    self.userEpisodes[episode!.objectId!] = object
+                    /*if let seen = object!["seen"] as? Bool {
+                        let episode = object!["episodio"] as? PFObject
+                        self.episodeState[episode!.objectId!] = seen
+                    }*/
+                }
+            }
+        }
+    }
+    
     //#Mark ------------------------------------------------------------------------------------------------------------------------
     //#Mark: - Table view data source
     //#Mark ------------------------------------------------------------------------------------------------------------------------
     
     /*override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
+    return 1
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.episodes.count
+    return self.episodes.count
     }
     */
     
@@ -94,16 +117,101 @@ class MyEpisodesViewController: PFQueryTableViewController {
     
     override func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [AnyObject]?  {
         
+        let episode: PFObject = self.objects![indexPath.row] as! PFObject
+        let episodeId: String = episode.objectId! as String
+        let userEpisode: PFObject = self.userEpisodes[episodeId] as! PFObject
+        let seen: Bool = userEpisode["seen"] as! Bool
+        
+        if seen {
+            var unwatchedAction = UITableViewRowAction(style: UITableViewRowActionStyle.Default, title: "Unwatched" , handler: { (action:UITableViewRowAction!, indexPath:NSIndexPath!) -> Void in
+                userEpisode.setValue(false, forKey: "seen")
+                userEpisode.saveInBackgroundWithBlock{
+                    (success: Bool, error: NSError?) -> Void in
+                    if (success) {
+                        self.userEpisodes[episodeId] = userEpisode
+                    } else {
+                        println(error)
+                    }
+                }
+                self.tableView.reloadData()
+            })
+            
+            return [unwatchedAction]
+        }
+        
         var watchedAction = UITableViewRowAction(style: UITableViewRowActionStyle.Default, title: "Watched" , handler: { (action:UITableViewRowAction!, indexPath:NSIndexPath!) -> Void in
-            
-            // Query
-            
+            userEpisode.setValue(true, forKey: "seen")
+            userEpisode.saveInBackgroundWithBlock{
+                (success: Bool, error: NSError?) -> Void in
+                if (success) {
+                    self.userEpisodes[episodeId] = userEpisode
+                } else {
+                    println(error)
+                }
+            }
             self.tableView.reloadData()
         })
         
         return [watchedAction]
+        
+        /*if let seen = episode["seen"] as? Bool {
+            var unwatchedAction = UITableViewRowAction(style: UITableViewRowActionStyle.Default, title: "Unwatched" , handler: { (action:UITableViewRowAction!, indexPath:NSIndexPath!) -> Void in
+                
+                var query = PFQuery(className: "UserHistorial")
+                query.whereKey("episodio", equalTo: episode)
+                query.whereKey("user", equalTo: PFUser.currentUser()!)
+                query.getFirstObjectInBackgroundWithBlock{
+                    (object: PFObject?, error: NSError?) -> Void in
+                    if error == nil && object != nil {
+                        object!.setValue(false, forKey: "seen")
+                        object!.saveInBackgroundWithBlock{
+                            (success: Bool, error: NSError?) -> Void in
+                            if (success) {
+                                
+                            } else {
+                                println(error)
+                            }
+                        }
+                    } else {
+                        println(error)
+                    }
+                }
+                
+                self.tableView.reloadData()
+            })
+            
+            return [unwatchedAction]
+        }
+        
+        var watchedAction = UITableViewRowAction(style: UITableViewRowActionStyle.Default, title: "Watched" , handler: { (action:UITableViewRowAction!, indexPath:NSIndexPath!) -> Void in
+            
+            var query = PFQuery(className: "UserHistorial")
+            query.whereKey("episodio", equalTo: episode)
+            query.whereKey("user", equalTo: PFUser.currentUser()!)
+            query.getFirstObjectInBackgroundWithBlock{
+                (object: PFObject?, error: NSError?) -> Void in
+                if error == nil && object != nil {
+                    object!.setValue(true, forKey: "seen")
+                    object!.saveInBackgroundWithBlock{
+                        (success: Bool, error: NSError?) -> Void in
+                        if (success) {
+                            
+                        } else {
+                            println(error)
+                        }
+                    }
+                } else {
+                    println(error)
+                }
+            }
+            
+            self.tableView.reloadData()
+        })
+        
+        return [watchedAction]*/
+        
     }
-
+    
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
     }
     
